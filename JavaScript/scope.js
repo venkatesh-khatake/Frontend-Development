f223364a4accb01415f20174bce31c172da954d0
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-05-01 20:27:00 +0530

    JS

--- JavaScript/scope.js
+++ JavaScript/scope.js
@@ -1,10 +1,19 @@
-function print(){
-   {
-    var name = "Venkey";
-    console.log("Inside the block",name);
-   }
-   console.log("Inside the Function : ",name);
+// function print(){
+//    {
+//     var name = "Venkey";
+//     console.log("Inside the block",name);
+//    }
+//    console.log("Inside the Function : ",name);
 
+// }
+// print()
+// console.log("Outside the function : ",name)
+
+var a = 10;
+
+function abc(){
+   console.log(a);
+   var a = 20;
 }
-print()
-console.log("Outside the function : ",name)
+
+abc()
