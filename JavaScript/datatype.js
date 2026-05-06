dee59843aa518f71e0f228faba4c6d73a30cffca
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-05-06 20:29:20 +0530

    completed Data types

---
+++ JavaScript/datatype.js
@@ -0,0 +1,13 @@
+let a = 22;
+let name = "Venkey";
+
+let userName = name + a;
+console.log(userName)
+console.log(typeof(userName))
+
+let x = 22;
+let y = "24";
+let z = "15"
+console.log(x + y)
+console.log(x - z)
+console.log(x * z)
