b7415ba5e43f008848e91792a4ef921d8b2616e1
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-05-11 21:14:14 +0530

    Loops

--- JavaScript/Loops.js
+++ JavaScript/Loops.js
@@ -1,52 +1,76 @@
-for(let i = 1; i<= 10; i++){
-    console.log(i)
-}
-console.log("--------------------------")
+// for(let i = 1; i<= 10; i++){
+//     console.log(i)
+// }
+// console.log("--------------------------")
 
-for(let i = 1; i<= 20; i++){
-    if(i % 2 == 0){
-        console.log(i)
-    }
-}
+// for(let i = 1; i<= 20; i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }
 
-console.log("==========================")
+// console.log("==========================")
 
-let sum = 0;
-for(let i = 1; i<= 100; i++){
-    sum += i;
-}
-console.log("Sum of 1 to 100 : ",sum)
+// let sum = 0;
+// for(let i = 1; i<= 100; i++){
+//     sum += i;
+// }
+// console.log("Sum of 1 to 100 : ",sum)
 
-let num = 5;
+// let num = 5;
 
-for(let i = 1; i<= 10; i++){
-    console.log(num * i)
-}
-
-
-for(let i = 10; i>= 1; i--){
-    console.log(i)
-}
-
-let str = "javascript";
-count = 0;
-for(let i = 0; i<= str.length; i++){
-    if(str[i] == 'a'|| str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
-        console.log(str[i])
-        count++;
-    }
-}
-console.log("count of Vowels : ",count)
+// for(let i = 1; i<= 10; i++){
+//     console.log(num * i)
+// }
 
 
-for(let i = 1; i<= 20; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log("FizzBuzz");
-    }else if(i % 3 == 0){
-        console.log("Fizz");
-    }else if(i % 5 == 0){
-        console.log("Buzz");
-    }else{
-        console.log(i)
-    }
+// for(let i = 10; i>= 1; i--){
+//     console.log(i)
+// }
+
+// let str = "javascript";
+// count = 0;
+// for(let i = 0; i<= str.length; i++){
+//     if(str[i] == 'a'|| str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+//         console.log(str[i])
+//         count++;
+//     }
+// }
+// console.log("count of Vowels : ",count)
+
+
+// for(let i = 1; i<= 20; i++){
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log("FizzBuzz");
+//     }else if(i % 3 == 0){
+//         console.log("Fizz");
+//     }else if(i % 5 == 0){
+//         console.log("Buzz");
+//     }else{
+//         console.log(i)
+//     }
+// }
+// ================================================
+
+
+
+
+
+// for(let i = 1; i<= 5; i++){
+//     let pattern = "";
+
+//     for(let j = 1; j <= i; j++){
+//       pattern += "*";
+//     }
+
+//     console.log(pattern)
+// }
+
+
+
+let num = 2;
+
+while(num == 10){
+   console.log(num);
+   num++;
 }
