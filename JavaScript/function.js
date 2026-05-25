9fa3784887e537b0df36451737b01392c3f0f78c
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-05-25 22:28:09 +0530

    learned about rest Parameter

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -88,3 +88,14 @@ function sum(a = 0  , b = 0){
 }
 
 console.log(sum(5,4))
+
+
+function sum(...val){
+    let sum = 0;
+    for(let i = 0; i <= val.length-1; i++){
+        sum += val[i];
+    }
+    return sum;
+}
+
+console.log(sum(1,2,3,4,5,6,7,8,9,10))
