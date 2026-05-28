fae249af05aa0cc8144299858d271e2abc30c99d
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-05-28 21:21:29 +0530

    learned about callback function

--- JavaScript/PureNimpure.js
+++ JavaScript/PureNimpure.js
@@ -1,22 +1,72 @@
 // example of Pure function 
 
-function sum(a,b){
-    return a + b;
+// function sum(a,b){
+//     return a + b;
+// }
+
+// console.log(3,3)
+
+// // example of Impure function
+
+// let count = 1;
+
+// function increment(){
+//     count++;
+   
+// }
+// increment()
+// increment()
+// increment()
+// increment()
+
+// console.log(count)
+
+
+
+function acceptMultiNum(...num){
+    console.log(...num);
+}
+acceptMultiNum(1,2,3,4,5,6)
+
+function add(...num){
+let sum = 0;
+    for(let i = 0; i<= num.length-1; i++){
+        sum += num[i]
+    }
+    return sum;
+}
+console.log(add(1,2,3,4,5))
+
+let arr1 = [1,2,3];
+let arr2 = [4,5,6];
+let arr3 = [...arr1, ...arr2];
+console.log(arr3)
+
+
+let greet = ()=>{
+    console.log("Hello")
+}
+greet()
+
+function processUser(fn){
+    fn();
+}
+processUser(greet)
+
+function multiply(a,b){
+    return a * b;
 }
 
-console.log(3,3)
-
-// example of Impure function
-
-let count = 1;
+let count = 0;
 
 function increment(){
-    count++;
-    return count;
+   return count++;
 }
-increment()
-increment()
-increment()
-increment()
-
+increment();
 console.log(count)
+increment();
+console.log(count)
+
+
+
+
---
+++ JavaScript/callback.js
@@ -0,0 +1,16 @@
+function calculator(a,b,operation){
+   return operation(a,b);
+}
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+
+console.log(calculator(3,4,multiply))
