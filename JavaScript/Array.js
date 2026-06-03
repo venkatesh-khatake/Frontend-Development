1bc783a008ec85edf78a30c1d2d758739e4f4bc9
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-06-03 16:06:11 +0530

    Started learning array

---
+++ JavaScript/Array.js
@@ -0,0 +1,16 @@
+let arr = ["Apple","Banana","Mango"];
+console.log(arr)
+
+arr[3] = 'Grapes';
+console.log(arr)   //add a new value at  the place of  defined index
+
+
+arr[1] = "Lichi";
+
+arr.push("Dragon Fruit","papaya") // adds value at the end of an array
+console.log(arr);
+
+ let removed = arr.pop(); // removes last element from an array
+ console.log(removed);
+
+
