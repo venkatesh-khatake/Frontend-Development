b5be852a4ebcd0036df9ee8d3984db3420d72dc8
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-06-06 20:42:20 +0530

    Learned about some(), every(), find() methods

--- JavaScript/mapFilterReduce.js
+++ JavaScript/mapFilterReduce.js
@@ -1,47 +1,73 @@
-// Map()
+// // Map()
 
-let arr = [10,20,30,40];
+// let arr = [10,20,30,40];
 
-let arr2 = arr.map(function(val,index){
-    return val * 2;
+// let arr2 = arr.map(function(val,index){
+//     return val * 2;
+// })
+// console.log(arr2)
+
+// let arr3 = arr.map(function(val,index){
+//     return val + index;
+// })
+// console.log(arr3)
+
+
+// // filter()
+
+// let num = [1,2,3,4,5,6,7,8];
+
+// let even = num.filter(function(val){
+//     return val % 2 === 0
+// })
+
+// console.log(even)
+
+// let odd = num.filter(function(val){
+//     return val % 2 !== 0
+// })
+
+// console.log(odd)
+
+// const users = [
+//     { name: "John", active: true },
+//     { name: "Alice", active: false },
+//     { name: "Bob", active: true }
+// ];
+
+// let activeUser = users.filter(function(user){
+//     return user.active;
+// })
+// console.log(activeUser);
+
+// let inactiveUser = users.filter(function(user){
+//     return user.active == false;
+// })
+
+// console.log(inactiveUser)
+
+// some()
+
+let numbers = [1,2,3,4,5,6];
+
+let result = numbers.some(function(num){
+    console.log(num)
+    return num * 10 === 50;
 })
-console.log(arr2)
+console.log(result)
 
-let arr3 = arr.map(function(val,index){
-    return val + index;
-})
-console.log(arr3)
+// every()
+let result2 = numbers.every(num =>num % 2 === 0);
+console.log(result2)
 
 
-// filter()
-
-let num = [1,2,3,4,5,6,7,8];
-
-let even = num.filter(function(val){
-    return val % 2 === 0
-})
-
-console.log(even)
-
-let odd = num.filter(function(val){
-    return val % 2 !== 0
-})
-
-console.log(odd)
-
+// find()
 const users = [
-    { name: "John", active: true },
-    { name: "Alice", active: false },
-    { name: "Bob", active: true }
+  { id: 1, name: "John" },
+  { id: 2, name: "Alice" }
 ];
 
-let activeUser = users.filter(function(user){
-    return user.active;
+result3 = users.find(function(user){
+    return user.id === 2;
 })
-console.log(activeUser);
-
-let inactiveUser = users.filter(function(user){
-    return user.active == false;
-})
-
-console.log(inactiveUser)
+console.log(result3)
