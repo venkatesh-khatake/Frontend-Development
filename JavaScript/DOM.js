cc3c6d0822aaf42f36b07b6d1a3511fefec10f8a
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-06-12 20:22:06 +0530

    Learned about Selectors & innerText

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -1,5 +1,24 @@
-let heading = document.getElementById("heading");
-console.log(heading)
+// let heading = document.getElementById("heading");
+// console.log(heading)
 
-let h1 = document.getElementsByClassName('h1');
-console.log(h1)
+// let h1 = document.getElementsByClassName('h1');
+// console.log(h1)
+
+
+
+let courseName = document.getElementById('course');
+console.log(courseName)
+
+let q = document.querySelector('#course')
+console.log(q.innerText)
+
+let qe = document.querySelectorAll('.courses')
+console.log(qe)
+// for(let i = 0;i < qe.length; i++){
+//     console.log(qe[i].innerText)
+// }
+
+
+qe.forEach(function(course,index){
+    console.log(index , course.innerText)
+})
