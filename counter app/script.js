a5b80cffe17b6d260576602febc9a15a8ee05b43
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-06-21 17:57:00 +0530

    Completed first counter app project on DOM

---
+++ counter app/script.js
@@ -0,0 +1,35 @@
+const countDisp = document.querySelector("#count");
+const decrease = document.querySelector("#decrease");
+const increase = document.querySelector("#increase");
+const reset = document.querySelector("#reset");
+
+let count =0;
+
+
+decrease.addEventListener('click',function(){
+   count--;
+    countDisp.innerText = count;
+})
+
+increase.addEventListener('click',function(){
+    count++;
+    countDisp.innerText = count;
+})
+
+reset.addEventListener('click',function(){
+    count = 0;
+    countDisp.innerText = count;
+})
+
+function updateCount() {
+    countDisp.innerText = count;
+
+    if (count > 0) {
+        countDisp.style.color = "green";
+    } else if (count < 0) {
+        countDisp.style.color = "red";
+    } else {
+        countDisp.style.color = "black";
+    }
+}
+
