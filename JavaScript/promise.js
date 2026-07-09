d03142005eb7ec14c5d85c0488edb3921338c8a0
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-07-09 20:10:50 +0530

    Learned about Promise.all

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -27,14 +27,45 @@
 // }
 
 
-console.log("Start");
+// console.log("Start");
 
-setTimeout(()=>{
-    console.log("Timer");
-},0);
+// setTimeout(()=>{
+//     console.log("Timer");
+// },0);
 
-Promise.resolve().then(()=>{
-    console.log("Promise");
-});
+// Promise.resolve().then(()=>{
+//     console.log("Promise");
+// });
 
-console.log("End")
+// console.log("End")
+
+
+
+// console.log("Start");
+
+// const promise = new Promise((resolve) => {
+//     console.log("Inside Executor");
+//     resolve("Success");
+//     console.log("After Resolve");
+// });
+
+// promise.then((result) => {
+//     console.log(result);
+// });
+
+// console.log("End");
+
+
+
+const p1 = Promise.resolve("HTML");
+const p2 = Promise.resolve("CSS");
+const p3 = Promise.resolve("JS");
+const p4 = Promise.resolve("React")
+
+Promise.all([p1,p2,p3,p4])
+.then((result)=>{
+    console.log(result);
+})
+.catch((error)=>{
+    console.log(error)
+})
