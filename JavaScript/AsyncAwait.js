e3201e1062dbe8223570fcbe0eb836d4b3c99a50
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2026-07-18 20:11:33 +0530

    Started learning Async await

---
+++ JavaScript/AsyncAwait.js
@@ -0,0 +1,7 @@
+async function getNumber(){
+    return 100;
+}
+
+const result = getNumber();
+
+console.log(result);
--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,8 +1,42 @@
- fetch("https://jsonplaceholder.typicode.com/users")
+//  fetch("https://jsonplaceholder.typicode.com/users")
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data.status )
+// })
+
+
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+// fetch(url)
+// .then((response)=>{
+//     return response.json();
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=>{
+//     console.log("Something went wrong")
+//     console.log(error)
+// })
+
+fetch(url)
 .then((response)=>{
-    return response.json()
+    if(!response.ok){
+        throw new Error(
+            `HTTP Error : ${response.status}`
+        )
+
+    }
+    return response.json();
 })
 .then((data)=>{
-    console.log(data.status )
+    console.log(data);
 })
+.catch((error)=>{
+    console.log(" : ",error.message);
+    console.log(error.status)
 
+})
